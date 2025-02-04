af74f53e156a62d959ca28c749ab6c8817e464b8
Ghxst727 <mohsinm7653@nycstudents.net>
2025-02-04 16:25:25 +0000

    2/4/25

--- LAB1/challenge/script.js
+++ LAB1/challenge/script.js
@@ -1,4 +1,4 @@
-
+const prompt = require('prompt-sync')();
 // Mild Challenges 
 /// The three siblings get their allowance once a week.
 /// Their current obsession is buying unique card games and collecting shoes.
@@ -47,15 +47,18 @@ console.log(sibling3.substring(16, 26));
 // 12. Rewrite these function as an arrow function.
 /// Then, call them with different inputs to check the result.
 
-function getRandomNum() {
-    return Math.floor(Math.random() * 100);
-}
 
-function greeting(name) {
+const getRandomNum = (x) => {
+    return Math.floor(Math.random(x) * 100);
+}
+console.log(getRandomNum(5));
+
+const greeting = (name) => {
     return `Hello ${name}, I'm glad you can make it!`;
 }
+console.log(greeting("Mohsir"));
 
-function perfectRoot(x) {
+const perfectRoot = (x) => {
     let root = Math.sqrt(x);
     if (Math.floor(root) === root) {
         return root;
@@ -63,6 +66,7 @@ function perfectRoot(x) {
         return "No perfect root"
     }
 }
+console.log(perfectRoot(49));
 
 // 13. Fix/Finish the following functions.
 /// Then, call them with at least 2 different inputs to ensure they work.
@@ -70,42 +74,45 @@ function perfectRoot(x) {
 /// a. Adds 2 numbers and returns the sum
 const addNums = (x, y) => {
     let sum = x + y;
+    return sum;
 }
-// uncomment the line below, to call the function, and add one more function call
-// console.log( addNums(4, 6) );
-
-
+console.log(addNums(5, 5));
+console.log(addNums(3, 8));
 /// b. Prints a special message if name is "Beyonce"
 const isBeyonce = (name) => {
-    // if( ) { 
-    //     return "Welcome Queen!";
-    // } else {
-    //     return `Sorry {name}, you're not Beyonce`;
-    // }
+    if(name == "Beyonce") { 
+        return "Welcome Queen!";
+    } else {
+        return `Sorry ${name}, you're not Beyonce`;
+    }
 }
 // Test "Beyonce" and other names to ensure it works
-
-
-
+console.log(isBeyonce("Beyonce"));
+console.log(isBeyonce("Mohsir"));
 /// c. If the number is even, return the number divided by 2, otherwise, return the number
 /// For example reduceEvens(10) => 5 -- reduceEvens(11) => 11
 const reduceEvens = (x) => {
-    // if( ){
-
-    // } else {
-    //     return x;
-    // }
+    if(x%2 == 0){
+        return x/2;
+    } else {
+        return x;
+    }
 }
 //Write your own function calls
-
+console.log(reduceEvens(8));
+console.log(reduceEvens(9));
 
 // 14. Write a function called shortenString(string) that returns the first 10 characters of a string if it is longer than 10
 //remove all
 const shortenString = (string) => {
-
+    if(string.length > 10){
+        return string.substring(0, 10)
+    } else{
+        return string;
+    }
 }
-
-
+console.log(shortenString("12345678910"));
+console.log(shortenString("123456789"));
 
 // Spicy Challenges
 let burger = 5.00;
@@ -117,25 +124,58 @@ let soda = 1.00;
 /// If no burgers, fries, and sodas can be bought, return "Sorry, no burgers for you."
 
 const buyBurgers = (budget) => {
-
+    if(isNaN(budget)){
+        return "Please enter a valid number";
+    }else if(budget >= 5){
+        return `You can buy ${Math.trunc(budget/burger)} burgers.`;
+    }else if(budget<5 && budget>=1){
+        return "Sorry, no burgers but you can still get fries or soda."
+    }else{
+        return "Sorry, no burgers for you."
+    }
 }
+console.log(buyBurgers(prompt("What's your budget?")));
 
 // 16. A meal consists of 1 burger, 1 fry, and 1 soda.
 /// Write a function called buyMeals(budget) that tells the user how many meals they can buy and how much money they will have left over.
 // Prompt the user for their budget and call the function.
 /// Bonus: Round the answer to the nearest penny.
 const buyMeals = (budget) => {
-
+    let meal = burger+fries+soda;
+    let numOfMeals = Math.trunc(budget/meal);
+    let change = budget-meal*numOfMeals;
+    return `You can buy ${numOfMeals} meals and have $${change} left over.`;
 }
-
+console.log(buyMeals(prompt("What's your budget?")));
 
 // 17. [Challenge] Write a function missingLeg(side1, side2) that takes in the length of a hypotenuse and a leg of a right triangle and prints out the missing leg's length
 // Prompt the user for the two sides and call the function
 /// The larger side should be the hypotenuse  
 
-
+const missingLeg = (side1, side2) => {
+    if(side1>side2){
+        return Math.sqrt((side1**2)-(side2**2));
+    } else if(side2>side1){
+        return Math.sqrt((side2**2)-(side1**2));
+    }else{
+        return "The hypotenuse should be longer than the other side.";
+    }
+}
+console.log(missingLeg(prompt('Give me one side'), prompt('Give me another side')))
 
 // 18. [Super Challenge] In math, Factorial is the product of an integer and all the integers below it.
 /// For example: factorial(3) = 3*2*1 = 6 ---  factorial(5) = 5*4*3*2*1 = 120
 /// Write a function factorial(x) that finds the factorial of any integer.
 // Prompt the user for a number and call the function
+const factorial = (x) => {
+    let y = 1;
+    if(isNaN(x)){
+        return "Make sure it's a number please."
+    }else{
+        for (let i = 1; i <= x; i++) {
+        y *= i;
+        }
+        return y;
+    }
+}
+console.log(factorial(prompt("Give me a number.")));
